46253d92de2780612262b701de712382f6a55d6f
Eric De Carufel <eric.decarufel@gsoft.com>
2023-08-17 15:46:30 -0400

    add sha

--- src/HelloPublisher/Program.cs
+++ src/HelloPublisher/Program.cs
@@ -1,17 +1,19 @@
-﻿using Azure;
+using Azure;
 using Azure.Identity;
 using Azure.Messaging.EventGrid;
 
 var client1 = new EventGridPublisherClient(
-    new Uri("https://localhost:5000/comments-eg/api/events"),
-    new AzureKeyCredential("dummy"));
-
-var client2 = new EventGridPublisherClient(
-    new Uri("https://localhost:5000/activities-eg/api/events"),
+    new Uri("https://localhost:6000/webhook-200"),
     new AzureCliCredential());
+
+// var client2 = new EventGridPublisherClient(
+//    new Uri("https://localhost:6000/activities-eg/api/events"),
+//    new AzureCliCredential());
 
 var data = new Dictionary<string, string>();
 data["foo"] = "bar";
 
-client1.SendEvent(new EventGridEvent("thesubject", "theeventtype", "1.0", data));
-client2.SendEvent(new EventGridEvent("thesubject", "theeventtype", "1.0", data));
+Response response = client1.SendEvent(new EventGridEvent("thesubject", "theeventtype", "1.0", data));
+Console.WriteLine(response.Status);
+Console.ReadLine();
+// client2.SendEvent(new EventGridEvent("thesubject", "theeventtype", "1.0", data));
