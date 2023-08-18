445dabfbf9250df763ad80b9af1e1a9360d5f056
Eric De Carufel <eric.decarufel@gsoft.com>
2023-08-18 08:43:18 -0400

    Fix some PR feedback

--- src/HelloPublisher/Program.cs
+++ src/HelloPublisher/Program.cs
@@ -6,14 +6,9 @@ var client1 = new EventGridPublisherClient(
     new Uri("https://localhost:6000/webhook-200"),
     new AzureCliCredential());
 
-// var client2 = new EventGridPublisherClient(
-//    new Uri("https://localhost:6000/activities-eg/api/events"),
-//    new AzureCliCredential());
-
 var data = new Dictionary<string, string>();
 data["foo"] = "bar";
 
 Response response = client1.SendEvent(new EventGridEvent("thesubject", "theeventtype", "1.0", data));
 Console.WriteLine(response.Status);
 Console.ReadLine();
-// client2.SendEvent(new EventGridEvent("thesubject", "theeventtype", "1.0", data));
