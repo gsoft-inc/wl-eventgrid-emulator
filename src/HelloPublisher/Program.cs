93c603f372cb35fa03147d3549f4d0a0f0cb0ed5
Eric De Carufel <decarufe@users.noreply.github.com>
2023-08-25 15:50:54 -0400

    Feature/idp 551 refactor eventgridemulator (#3)
    * WIP
    
    * Remove missing docker-compose in solution
    
    * changed compose execution context
    
    * Working docker compose with https
    
    * Fix Emulator calling host with http instead of https
    
    * Publish container on Azure
    
    * Six SemGrep issues
    
    * Revert Semgrep fix
    
    * Exclude semgrep rule yaml.docker-compose.security.writable-filesystem-service.writable-filesys
    
    * Revert Semgrep fix
    
    * Exclude semgrep rule
    
    * Fix reviews issues
    
    * Fix review issues
    
    * fix semgrep issues
    
    * revert readme chanes
    
    ---------
    
    Co-authored-by: Guillaume Caya-Letourneau <guillaume.caya@gsoft.com>

--- src/HelloPublisher/Program.cs
+++ src/HelloPublisher/Program.cs
@@ -3,12 +3,18 @@ using Azure.Identity;
 using Azure.Messaging.EventGrid;
 
 var client1 = new EventGridPublisherClient(
-    new Uri("http://localhost:8080/comments-eg/api/events"),
+    new Uri("https://localhost:6500/activities-eg/api/events"),
+    new AzureCliCredential());
+
+var client2 = new EventGridPublisherClient(
+    new Uri("https://localhost:6500/comments-eg/api/events"),
     new AzureCliCredential());
 
 var data = new Dictionary<string, string>();
 data["foo"] = "bar";
 
-Response response = client1.SendEvent(new EventGridEvent("thesubject", "theeventtype", "1.0", data));
-Console.WriteLine(response.Status);
-Console.ReadLine();
+Response response1 = client1.SendEvent(new EventGridEvent("thesubject", "theeventtype", "1.0", data));
+Console.WriteLine(response1.Status);
+
+Response response2 = client2.SendEvent(new EventGridEvent("thesubject", "theeventtype", "1.0", data));
+Console.WriteLine(response2.Status);
