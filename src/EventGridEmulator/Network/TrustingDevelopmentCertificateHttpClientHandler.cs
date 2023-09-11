f2270b2e88a5337077018de3acd58c4656753e66
Anthony Simmon <anthony.simmon@gsoft.com>
2023-09-11 18:09:43 -0400

    Typo

--- src/EventGridEmulator/Network/TrustingDevelopmentCertificateHttpClientHandler.cs
+++ src/EventGridEmulator/Network/TrustingDevelopmentCertificateHttpClientHandler.cs
@@ -5,7 +5,7 @@ internal sealed class TrustingDevelopmentCertificateHttpClientHandler : HttpClie
     public TrustingDevelopmentCertificateHttpClientHandler()
     {
         // This might seems like a security issue, but it's not.
-        // We can't trust subscribers' certificates as this emulator is intented to be used in a local environment,
+        // We trust the subscribers' certificates as this emulator is intented to be used in a local environment,
         // where certificates are mostly self-signed and not trusted by the Docker container.
         this.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
     }
