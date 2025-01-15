197abd0a6a0e0bcffaf13cf009e1336de330490c
David Riddervold Marconis <david.marconis@volue.com>
2025-01-15 12:32:00 +0100

    Add equals and hashcode implementations of filter

--- src/EventGridEmulator/Configuration/Filter.cs
+++ src/EventGridEmulator/Configuration/Filter.cs
@@ -4,17 +4,39 @@ namespace EventGridEmulator.Configuration;
 /// This Filter class is based on the event filtering in event grid:
 /// https://learn.microsoft.com/en-us/azure/event-grid/event-filtering#event-type-filtering
 /// </summary>
-public record Filter
+public class Filter
 {
-    public string[]? IncludedEventTypes { get; set; }
-
     public Filter(Filter other)
     {
         this.IncludedEventTypes = other.IncludedEventTypes?.ToArray();
     }
 
+    public Filter() { }
+
+    public string[]? IncludedEventTypes { get; set; }
+
     public override string ToString()
     {
         return $"Filter {{ IncludedEventTypes = [{string.Join(", ", this.IncludedEventTypes ?? [])}] }}";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Filter other)
+        {
+            return false;
+        }
+
+        return (this.IncludedEventTypes, other.IncludedEventTypes) switch
+        {
+            (null, null) => true,
+            (not null, not null) => this.IncludedEventTypes.SequenceEqual(other.IncludedEventTypes),
+            _ => false,
+        };
+    }
+
+    public override int GetHashCode()
+    {
+        return this.IncludedEventTypes == null ? 0 : this.IncludedEventTypes.GetHashCode();
     }
 }
